5b6fa2c490e80bb0a0dab45e1fc1c28f1c5183e4
dlopes7 <davidribeirolopes@gmail.com>
2020-06-21 13:02:19 -0300

    Add frequency

--- third-party-synthetic/extension-external-ping/src/ping_extension.py
+++ third-party-synthetic/extension-external-ping/src/ping_extension.py
@@ -12,6 +12,7 @@ class PingExtension(RemoteBasePlugin):
     def initialize(self, **kwargs):
         # The Dynatrace API client
         self.client = DynatraceAPI(self.config.get("api_url"), self.config.get("api_token"), log=log)
+        self.executions = 0
 
     def query(self, **kwargs) -> None:
 
@@ -20,18 +21,22 @@ class PingExtension(RemoteBasePlugin):
         name = self.config.get("test_name")
         target = self.config.get("test_target")
         location = self.config.get("test_location", "") if self.config.get("test_location") else "ActiveGate"
+        frequency = int(self.config.get("frequency", 1))
 
-        ping_result = ping(target)
-        log.info(ping_result.as_dict())
+        if self.executions % frequency == 0:
+            ping_result = ping(target)
+            log.info(ping_result.as_dict())
 
-        self.client.report_simple_test(
-            name=name, location_name=location, success=ping_result.packet_loss_rate == 0, response_time=ping_result.rtt_avg or 0,
-        )
+            self.client.report_simple_test(
+                name=name, location_name=location, success=ping_result.packet_loss_rate == 0, response_time=ping_result.rtt_avg or 0,
+            )
 
-        if ping_result.packet_loss_rate > 0:
-            self.client.report_simple_event(name, f"Ping failed for {name}, target: {target}", location)
-        else:
-            self.client.report_simple_event(name, f"Ping failed for {name}, target: {target}", location, state="resolved")
+            if ping_result.packet_loss_rate > 0:
+                self.client.report_simple_event(name, f"Ping failed for {name}, target: {target}", location)
+            else:
+                self.client.report_simple_event(name, f"Ping failed for {name}, target: {target}", location, state="resolved")
+
+        self.executions += 1
 
 
 def ping(host: str) -> pingparsing.PingStats:
