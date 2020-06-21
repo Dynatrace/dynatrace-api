a9fe9d3c125061bc3694e313eaec125abb7aa45a
dlopes7 <davidribeirolopes@gmail.com>
2020-06-21 12:43:16 -0300

    Add external ping extension

---
+++ third-party-synthetic/extension-external-ping/src/ping_extension.py
@@ -0,0 +1,43 @@
+import logging
+
+from ruxit.api.base_plugin import RemoteBasePlugin
+from dynatrace_api import DynatraceAPI
+
+import pingparsing
+
+log = logging.getLogger(__name__)
+
+
+class PingExtension(RemoteBasePlugin):
+    def initialize(self, **kwargs):
+        # The Dynatrace API client
+        self.client = DynatraceAPI(self.config.get("api_url"), self.config.get("api_token"), log=log)
+
+    def query(self, **kwargs) -> None:
+
+        log.setLevel(self.config.get("log_level"))
+
+        name = self.config.get("test_name")
+        target = self.config.get("test_target")
+        location = self.config.get("test_location", "") if self.config.get("test_location") else "ActiveGate"
+
+        ping_result = ping(target)
+        log.info(ping_result.as_dict())
+
+        self.client.report_simple_test(
+            name=name, location_name=location, success=ping_result.packet_loss_rate == 0, response_time=ping_result.rtt_avg or 0,
+        )
+
+        if ping_result.packet_loss_rate > 0:
+            self.client.report_simple_event(name, f"Ping failed for {name}, target: {target}", location)
+        else:
+            self.client.report_simple_event(name, f"Ping failed for {name}, target: {target}", location, state="resolved")
+
+
+def ping(host: str) -> pingparsing.PingStats:
+    ping_parser = pingparsing.PingParsing()
+    transmitter = pingparsing.PingTransmitter()
+    transmitter.destination = host
+    transmitter.count = 2
+    transmitter.timeout = 2000
+    return ping_parser.parse(transmitter.ping())
