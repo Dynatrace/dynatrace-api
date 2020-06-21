1b22bd440ab5dd8d3ce54ef1afe520f7ed320963
dlopes7 <davidribeirolopes@gmail.com>
2020-06-21 14:38:52 -0300

    Fix frequency

--- third-party-synthetic/extension-external-ping/src/ping_extension.py
+++ third-party-synthetic/extension-external-ping/src/ping_extension.py
@@ -21,7 +21,7 @@ class PingExtension(RemoteBasePlugin):
         name = self.config.get("test_name")
         target = self.config.get("test_target")
         location = self.config.get("test_location", "") if self.config.get("test_location") else "ActiveGate"
-        frequency = int(self.config.get("frequency", 1))
+        frequency = int(self.config.get("frequency") or 1)
 
         if self.executions % frequency == 0:
             ping_result = ping(target)
