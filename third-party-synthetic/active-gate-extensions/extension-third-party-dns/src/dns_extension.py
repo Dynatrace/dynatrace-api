5d5280d5c790e88cad882103ee47aca1d7d80471
florent-duchateau <48473194+florent-duchateau@users.noreply.github.com>
2021-04-19 13:57:45 +0100

    Update dns_extension.py

--- third-party-synthetic/active-gate-extensions/extension-third-party-dns/src/dns_extension.py
+++ third-party-synthetic/active-gate-extensions/extension-third-party-dns/src/dns_extension.py
@@ -51,7 +51,7 @@ class DNSExtension(RemoteBasePlugin):
             success, response_time = test_dns(dns_server, host)
             log.info(f"DNS test, DNS server: {dns_server}, host: {host}, success: {success}, time: {response_time} ")
 
-            self.dt_client.report_simple_thirdparty_synthetic_test(
+            self.dt_client.third_part_synthetic_tests.report_simple_thirdparty_synthetic_test(
                 engine_name="DNS",
                 timestamp=datetime.now(),
                 location_id=location_id,
@@ -66,7 +66,7 @@ class DNSExtension(RemoteBasePlugin):
                 icon_url="https://raw.githubusercontent.com/Dynatrace/dynatrace-api/master/third-party-synthetic/active-gate-extensions/extension-third-party-dns/dns.png",
             )
 
-            self.dt_client.report_simple_thirdparty_synthetic_test_event(
+            self.dt_client.third_part_synthetic_tests.report_simple_thirdparty_synthetic_test_event(
                 test_id=self.activation.entity_id,
                 name=f"DNS lookup failed for {step_title}",
                 location_id=location_id,
